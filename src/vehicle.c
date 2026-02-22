aa3ee629424e2e4a70284e1ae98b1a091322d275
Jeffrey Zhuang <jeffreyzhuangyt@gmail.com>
2026-02-22 13:39:15 -0500

    Move CLI from vehicle to main

--- src/vehicle.c
+++ src/vehicle.c
@@ -1,6 +1,7 @@
 #include "vehicle.h"
 
-void vehicle_init(vehicle_t *vehicle) {
+void vehicle_init(vehicle_t *vehicle, uart_t *debug_uart) {
+    vehicle->debug_uart = debug_uart;
     vehicle->led_timer = 0;
     vehicle->ins_timer = 0;
     vehicle->led_on = false;
@@ -9,15 +10,13 @@ void vehicle_init(vehicle_t *vehicle) {
     gpio_init(&board_pins.led);
     timer_init(&vehicle->servo_y, TIM1, &board_pins.tim1_ch4);
     timer_init(&vehicle->servo_z, TIM3, &board_pins.tim3_ch2);
-    uart_init(&vehicle->debug_uart, UART1, &board_pins.uart1_tx,
-        &board_pins.uart1_rx, 115200);
     spi_init(&vehicle->imu_spi, SPI1, &board_pins.spi1_cs,
         &board_pins.spi1_miso, &board_pins.spi1_mosi, &board_pins.spi1_sck);
     spi_init(&vehicle->flash_spi, SPI2, &board_pins.spi2_cs,
         &board_pins.spi2_miso, &board_pins.spi2_mosi, &board_pins.spi2_sck);
 
     ins_init(&vehicle->ins, &vehicle->imu_spi);
-    logger_init(&vehicle->logger, &vehicle->flash_spi, &vehicle->debug_uart);
+    logger_init(&vehicle->logger, &vehicle->flash_spi, vehicle->debug_uart);
 }
 
 void vehicle_update_flight(vehicle_t *vehicle) {
@@ -48,64 +47,6 @@ void vehicle_erase(vehicle_t *vehicle) {
     logger_erase_output(&vehicle->logger);
 }
 
-boot_mode_t vehicle_run_cli(vehicle_t *vehicle) {
-    uint8_t cmd_buf_len = 32;
-    char cmd_buf[cmd_buf_len];
-    uint8_t idx = 0;
-    uint32_t print_timer = 0;
-    uint32_t start_time = get_time();
-
-    for (;;) {
-        while (!uart_read_ready(&vehicle->debug_uart)) {
-            // Default to flight mode after 10 seconds
-            if (get_time() - start_time > 10000) {
-                return BOOT_MODE_FLIGHT;
-            }
-
-            if (timer_expired(&print_timer, 2000)) {
-                char uart_buf[100] = "Missile CLI\r\n"
-                    "(1) Flight\r\n(2) Calibrate\r\n(3) Fin Test\r\n"
-                    "(4) HITL\r\n(5) Retrieve\r\n(9) Erase\r\n";
-                uart_write_buf(&vehicle->debug_uart, uart_buf,
-                    strlen(uart_buf));
-            }
-        }
-
-        char c = uart_read_byte(&vehicle->debug_uart);
-
-        if (c == '\r' || c == '\n') {
-            cmd_buf[idx] = '\0';
-            idx = 0;
-
-            if (strcmp(cmd_buf, "1") == 0) {
-                uart_write_buf(&vehicle->debug_uart, "Flight Selected\r\n", 17);
-                return BOOT_MODE_FLIGHT;
-            } else if (strcmp(cmd_buf, "2") == 0) {
-                uart_write_buf(&vehicle->debug_uart, "Calibrate Selected\r\n",
-                    20);
-                return BOOT_MODE_CALIBRATE;
-            } else if (strcmp(cmd_buf, "5") == 0) {
-                uart_write_buf(&vehicle->debug_uart, "Retrieve Selected\r\n",
-                    19);
-                return BOOT_MODE_RETRIEVE;
-            } else if (strcmp(cmd_buf, "9") == 0) {
-                uart_write_buf(&vehicle->debug_uart, "Erase Selected\r\n", 16);
-                return BOOT_MODE_ERASE;
-            } else {
-                uart_write_buf(&vehicle->debug_uart, "Invalid\r\n", 9);
-            }
-
-            continue;
-        }
-
-        if (idx < (cmd_buf_len - 1)) {
-            cmd_buf[idx++] = c;
-        } else {
-            idx = 0;
-        }
-    }
-}
-
 void vehicle_print_state(vehicle_t *vehicle) {
     float roll;
     float pitch;
@@ -123,5 +64,5 @@ void vehicle_print_state(vehicle_t *vehicle) {
         (double)roll, (double)pitch, (double)yaw,
         (double)vehicle->ins.vel.x, (double)vehicle->ins.vel.y,
         (double)vehicle->ins.vel.z);
-    uart_write_buf(&vehicle->debug_uart, uart_buf, strlen(uart_buf));
+    uart_write_buf(vehicle->debug_uart, uart_buf, strlen(uart_buf));
 }
--- src/vehicle.h
+++ src/vehicle.h
@@ -13,34 +13,24 @@
 #include "logger.h"
 #include "board.h"
 
-typedef enum {
-    BOOT_MODE_FLIGHT,
-    BOOT_MODE_CALIBRATE,
-    BOOT_MODE_RETRIEVE,
-    BOOT_MODE_ERASE
-} boot_mode_t;
-
 typedef struct {
     ins_t ins;
     logger_t logger;
     spi_t imu_spi;
     spi_t flash_spi;
-    uart_t debug_uart;
     timer_t servo_y;
     timer_t servo_z;
+    uart_t *debug_uart;
     bool led_on;
     uint32_t led_timer;
     uint32_t ins_timer;
 } vehicle_t;
 
-void vehicle_init(vehicle_t *vehicle);
-
+void vehicle_init(vehicle_t *vehicle, uart_t *debug_uart);
 void vehicle_update_flight(vehicle_t *vehicle);
 void vehicle_update_calibrate(vehicle_t *vehicle);
 void vehicle_retrieve(vehicle_t *vehicle);
 void vehicle_erase(vehicle_t *vehicle);
-
-boot_mode_t vehicle_run_cli(vehicle_t *vehicle);
 void vehicle_print_state(vehicle_t *vehicle);
 
 #endif // VEHICLE_H
