02e390c09ca51732092706beece449bcbbb8cf10
Jeffrey Zhuang <jeffreyzhuangyt@gmail.com>
2026-02-23 22:21:05 -0500

    Servo_y working but not servo_z

--- test/integration/main.c
+++ test/integration/main.c
@@ -10,9 +10,14 @@ int main(void) {
         .af = 0,
         .mode = GPIO_MODE_OUTPUT
     };
-
     gpio_init(&led);
 
+    gpio_t tim3_ch2 = {
+        .gpio_reg = GPIOA,
+        .pin = 7,
+        .af = 1,
+        .mode = GPIO_MODE_AF
+    };
     gpio_t tim1_ch4 = {
         .gpio_reg = GPIOA,
         .pin = 11,
@@ -20,8 +25,10 @@ int main(void) {
         .mode = GPIO_MODE_AF
     };
 
-    timer_t servo;
-    timer_init(&servo, TIM1, &tim1_ch4, 4);
+    timer_t servo_y;
+    timer_t servo_z;
+    timer_init(&servo_y, TIM3, &tim3_ch2, 2);
+    timer_init(&servo_z, TIM1, &tim1_ch4, 4);
 
     bool led_on = false;
     uint32_t led_timer = 0;
@@ -30,9 +37,15 @@ int main(void) {
         if (timer_expired(&led_timer, 500)) {
             gpio_write(&led, led_on);
             led_on = !led_on;
-        }
 
-        timer_set_duty(&servo, 0.5);
+            if (led_on) {
+                timer_set_duty(&servo_y, 0.7f);
+                timer_set_duty(&servo_z, 0.5f);
+            } else {
+                timer_set_duty(&servo_y, 0.3f);
+                timer_set_duty(&servo_z, 0.6f);
+            }
+        }
     }
 
     return 0;
