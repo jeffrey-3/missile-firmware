5e6b997f64482e7e37ad0e971dbb6d95b17baddc
Jeffrey Zhuang <jeffreyzhuangyt@gmail.com>
2026-02-23 23:44:34 -0500

    Divide timer clock to 1 MHz

--- src/hal/timer.c
+++ src/hal/timer.c
@@ -12,22 +12,17 @@ void timer_init(timer_t *timer, struct tim_reg *tim_reg, gpio_t *gpio,
     // Enable peripheral clock
     if (timer->tim_reg == TIM1) {
         RCC->APBENR2 |= 1UL << 11;
-
-        // Enable main output
-        timer->tim_reg->BDTR |= 1UL << 15;
+        timer->tim_reg->BDTR |= 1UL << 15; // Enable main output
     } else if (timer->tim_reg == TIM3) {
         RCC->APBENR1 |= 1UL << 1;
     }
 
-    timer->tim_reg->PSC = 15; // Divide timer clock
+    timer->tim_reg->PSC = FREQ / 1000000 - 1; // Divide timer clock to 1 MHz
     timer->tim_reg->ARR = 3000; // Timer count to value from 0
 
     if (timer->channel == 4) {
-        // PWM Mode 1 (set OC4M to 0110)
-        timer->tim_reg->CCMR2 |= 3UL << 13;
-
-        // Enable output on channel 4 (set CC4E to 1)
-        timer->tim_reg->CCER |= 1UL << 12;
+        timer->tim_reg->CCMR2 |= 3UL << 13; // PWM Mode 1 (OC4M = 0110)
+        timer->tim_reg->CCER |= 1UL << 12; // Enable output on CH4 (CC4E = 1)
     } else if (timer->channel == 2) {
         timer->tim_reg->CCMR1 |= 3UL << 13;
         timer->tim_reg->CCER |= 1UL << 4;
