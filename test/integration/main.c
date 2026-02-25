eea75469641d867f97029790a0e17243a29410a2
Jeffrey Zhuang <jeffreyzhuangyt@gmail.com>
2026-02-25 12:28:41 -0500

    Remove delay from integration test and poll status register instead

--- test/integration/main.c
+++ test/integration/main.c
@@ -69,12 +69,11 @@ void test_erase_flash(void) {
 
     // Erase every sector one by one
     for (uint16_t i = 0; i < LOGGER_NUM_SECTORS; i++) {
-        // After every erase, the flash chip disables write, so must re-enable
+        while (w25q128jv_check_busy(&flash)) spin(1);
         w25q128jv_write_enable(&flash);
-        delay(5);
 
+        while (w25q128jv_check_busy(&flash)) spin(1);
         w25q128jv_erase_sector(&flash, i);
-        delay(500);
 
         char uart_buf[100];
         snprintf(uart_buf, sizeof(uart_buf), "Erased %d out of %d\r\n",
@@ -106,10 +105,11 @@ void test_read_flash(void) {
         uart_write_buf(&uart, buf, strlen(buf));
 
         // Read this page and get array of message structs
+        while (w25q128jv_check_busy(&flash)) spin(1);
         w25q128jv_write_disable(&flash);
-        delay(5);
 
         uint8_t data[LOGGER_PAGE_SIZE];
+        while (w25q128jv_check_busy(&flash)) spin(1);
         w25q128jv_read(&flash, i, 0, LOGGER_PAGE_SIZE, data);
         ring_buffer_write_arr(&ring_buffer, data, LOGGER_PAGE_SIZE);
 
