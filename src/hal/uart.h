526dde9abd404d178bb084d28daeae432ac6fe36
Jeffrey Zhuang <jeffreyzhuangyt@gmail.com>
2026-03-06 00:10:12 -0500

    Improve UART interface

--- src/hal/uart.h
+++ src/hal/uart.h
@@ -17,8 +17,8 @@ typedef struct {
 void uart_init(uart_t *uart, struct uart_reg *uart_reg, gpio_t *tx, gpio_t *rx,
     unsigned long baud);
 void uart_write(uart_t *uart, char *buf, size_t len);
-bool uart_empty(uart_t *uart);
 void uart_read(uart_t *uart, uint8_t* byte);
+bool uart_empty(uart_t *uart);
 void _uart1_irq_handler();
 
 #endif // UART_H
