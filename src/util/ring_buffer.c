926f7cc76d85ad34fed5907032ae57a67baf3066
Jeffrey Zhuang <jeffreyzhuangyt@gmail.com>
2026-03-06 02:31:15 -0500

    Add ring buffer float

--- src/util/ring_buffer.c
+++ src/util/ring_buffer.c
@@ -1,12 +1,18 @@
 #include "ring_buffer.h"
 
-void ring_buffer_setup(ring_buffer_t* rb) {
+void rb_setup_u8(rb_u8_t* rb) {
     rb->read_index = 0;
     rb->write_index = 0;
     rb->mask = RING_BUFFER_SIZE - 1;
 }
 
-uint32_t ring_buffer_read(ring_buffer_t* rb, uint8_t* data, uint32_t len) {
+void rb_setup_f32(rb_f32_t* rb) {
+    rb->read_index = 0;
+    rb->write_index = 0;
+    rb->mask = RING_BUFFER_SIZE - 1;
+}
+
+uint32_t rb_read_u8(rb_u8_t* rb, uint8_t* data, uint32_t len) {
     uint32_t count = 0;
 
     while (count < len) {
@@ -25,7 +31,26 @@ uint32_t ring_buffer_read(ring_buffer_t* rb, uint8_t* data, uint32_t len) {
     return count;
 }
 
-uint32_t ring_buffer_write(ring_buffer_t* rb, uint8_t *data, uint32_t len) {
+uint32_t rb_read_f32(rb_f32_t* rb, uint8_t* data, uint32_t len) {
+    uint32_t count = 0;
+
+    while (count < len) {
+        uint32_t local_read_index = rb->read_index;
+        uint32_t local_write_index = rb->write_index;
+
+        if (local_read_index == local_write_index) {
+            break; // Buffer is empty
+        }
+
+        data[count++] = rb->buffer[local_read_index];
+        local_read_index = (local_read_index + 1) & rb->mask;
+        rb->read_index = local_read_index;
+    }
+
+    return count;
+}
+
+uint32_t rb_write_u8(rb_u8_t* rb, uint8_t *data, uint32_t len) {
     uint32_t count = 0;
 
     while (count < len) {
@@ -45,6 +70,30 @@ uint32_t ring_buffer_write(ring_buffer_t* rb, uint8_t *data, uint32_t len) {
     return count;
 }
 
-uint32_t ring_buffer_count(ring_buffer_t* rb) {
+uint32_t rb_write_f32(rb_f32_t* rb, float *data, uint32_t len) {
+    uint32_t count = 0;
+
+    while (count < len) {
+        uint32_t local_write_index = rb->write_index;
+        uint32_t local_read_index = rb->read_index;
+
+        uint32_t next_write_index = (local_write_index + 1) & rb->mask;
+
+        if (next_write_index == local_read_index) {
+            break; // Buffer is full
+        }
+
+        rb->buffer[local_write_index] = data[count++];
+        rb->write_index = next_write_index;
+    }
+
+    return count;
+}
+
+uint32_t rb_count_u8(rb_u8_t* rb) {
+    return (rb->write_index - rb->read_index) & rb->mask;
+}
+
+uint32_t rb_count_f32(rb_f32_t* rb) {
     return (rb->write_index - rb->read_index) & rb->mask;
 }
--- src/util/ring_buffer.h
+++ src/util/ring_buffer.h
@@ -6,16 +6,30 @@
 
 #define RING_BUFFER_SIZE 1024 // Must be power of 2
 
-typedef struct ring_buffer_t {
+typedef struct {
     uint8_t buffer[RING_BUFFER_SIZE];
     uint32_t mask;
     uint32_t read_index;
     uint32_t write_index;
-} ring_buffer_t;
+} rb_u8_t;
 
-void ring_buffer_setup(ring_buffer_t* rb);
-uint32_t ring_buffer_write(ring_buffer_t* rb, uint8_t *data, uint32_t len);
-uint32_t ring_buffer_read(ring_buffer_t* rb, uint8_t* data, uint32_t len);
-uint32_t ring_buffer_count(ring_buffer_t* rb);
+typedef struct {
+    float buffer[RING_BUFFER_SIZE];
+    uint32_t mask;
+    uint32_t read_index;
+    uint32_t write_index;
+} rb_f32_t;
+
+void rb_setup_u8(rb_u8_t* rb);
+void rb_setup_f32(rb_f32_t* rb);
+
+uint32_t rb_write_u8(rb_u8_t* rb, uint8_t *data, uint32_t len);
+uint32_t rb_write_f32(rb_f32_t* rb, float *data, uint32_t len);
+
+uint32_t rb_read_u8(rb_u8_t* rb, uint8_t *data, uint32_t len);
+uint32_t rb_read_f32(rb_f32_t* rb, float *data, uint32_t len);
+
+uint32_t rb_count_u8(rb_u8_t* rb);
+uint32_t rb_count_f32(rb_f32_t* rb);
 
 #endif // RING_BUFFER_H
